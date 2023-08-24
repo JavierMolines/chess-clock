bef21ce22f8f80e233e5506beac1838be59c0ec9
JavierMolines <eljavimolines@gmail.com>
2023-08-24 06:17:58 -0300

    Update principal proyect

--- app/component/Icons/Play.tsx
+++ app/component/Icons/Play.tsx
@@ -3,11 +3,11 @@ export const IconPlay = () => {
 		// rome-ignore lint/a11y/noSvgWithoutTitle: <explanation>
 		<svg
 			xmlns="http://www.w3.org/2000/svg"
-			width="24"
-			height="24"
+			width="48"
+			height="48"
 			viewBox="0 0 24 24"
 			fill="none"
-			stroke="#694343"
+			stroke="#000000"
 			strokeWidth="2"
 			strokeLinecap="round"
 			strokeLinejoin="round"
---
+++ app/hooks/useMovement.ts
@@ -0,0 +1,113 @@
+import { useEffect, useState } from "react";
+
+const useMovement = () => {
+	const velocity = 1000;
+	const standard = {
+		second: 0,
+		minute: 10,
+		hour: 0,
+	};
+
+	// rome-ignore lint/suspicious/noExplicitAny: <explanation>
+	const [turn, setTurn] = useState<any>(null);
+	const [gameOn, setGameOn] = useState(false);
+	const [ownerTime, setOwnerTime] = useState(standard);
+	const [inviteTime, setInviteTime] = useState(standard);
+
+	const reduceTimer = (currentPlayer: boolean, initGame: boolean) => {
+		if (initGame === false && gameOn === false) return;
+
+		const assignNewTime = currentPlayer ? setOwnerTime : setInviteTime;
+		const { second, minute, hour } = currentPlayer ? ownerTime : inviteTime;
+		const newSecond = second - 1;
+		const newMinute = minute - 1;
+		const newHour = hour - 1;
+
+		// HAVE SECONDS
+		if (newSecond > 0) {
+			assignNewTime({
+				hour,
+				minute,
+				second: newSecond,
+			});
+			return;
+		}
+
+		// HAVE MINUTES
+		if (minute > 0) {
+			assignNewTime({
+				hour,
+				minute: newMinute,
+				second: 59,
+			});
+			return;
+		}
+
+		// HAVE HOURS
+		if (hour > 0) {
+			assignNewTime({
+				hour: newHour,
+				minute: 59,
+				second: 59,
+			});
+			return;
+		}
+
+		// END CLOCK
+		if (newSecond <= 0 && newMinute <= 0 && newHour <= 0) {
+			setGameOn(false);
+			assignNewTime({
+				hour: 0,
+				minute: 0,
+				second: 0,
+			});
+			return;
+		}
+
+		// NOT HAVE SECONDS BUT HAVE MINUTES
+		if (newSecond <= 0 && newMinute > 0) {
+			assignNewTime({
+				hour,
+				minute: newMinute,
+				second: 59,
+			});
+			return;
+		}
+
+		// NOT HAVE SECONDS AND NOT HAVE MINUTES BUT HAVE HOURS
+		if (newSecond <= 0 && newMinute <= 0 && newHour > 0) {
+			assignNewTime({
+				hour: newHour,
+				minute: 59,
+				second: 59,
+			});
+			return;
+		}
+	};
+
+	const handleTurn = (flow: "OWNER" | "VISIT") => {
+		if (!gameOn) {
+			setGameOn(true);
+		}
+		const initTurn = flow === "OWNER";
+		setTurn(!initTurn);
+		reduceTimer(!initTurn, true);
+		window.navigator.vibrate(100);
+	};
+
+	useEffect(() => {
+		if (gameOn) {
+			const idTimer = setTimeout(() => {
+				reduceTimer(turn, false);
+			}, velocity);
+
+			return () => {
+				clearTimeout(idTimer);
+			};
+		}
+	}, [ownerTime, inviteTime]);
+
+	return { gameOn, turn, ownerTime, inviteTime, handleTurn };
+};
+
+export { useMovement };
