62c4601805b60f34c717dc5f2746bdf26921ec12
JavierMolines <eljavimolines@gmail.com>
2025-01-04 18:05:31 -0300

    fix(type-add): insert type to setTimingPlay function

--- app/utils/storage.ts
+++ app/utils/storage.ts
@@ -1,6 +1,8 @@
+import type { IGameStore } from "../types/gameStore.type";
+
 const ID_TIME_PLAY = "timeToPlay";
 
-const setTimingPlay = (time) => {
+const setTimingPlay = (time: IGameStore) => {
 	localStorage.setItem(ID_TIME_PLAY, JSON.stringify(time));
 };
 
